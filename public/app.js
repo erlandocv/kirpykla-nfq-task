0b54ab52e6fccc5265f207dc75644088de8dffb5
Erlandas <30452017+erlandasz@users.noreply.github.com>
2019-02-19 17:55:31 +0200

    nearly done

--- public/app.js
+++ public/app.js
@@ -1,10 +1,14 @@
+/* global moment */
+
 var app = angular.module('app', ['ui.bootstrap']);
 
-app.controller('ReservationController', function ReservationController($scope, $http) {
+app.controller('ReservationController', function ReservationController($scope, $http, $q) {
 
-    $scope.reservation = {
-        name: '',
-    };
+    $scope.reservation = {};
+    $scope.reservations = [];
+    $scope.barbers = [];
+    $scope.times = [];
+    $scope.allTimes = [];
 
     $scope.datePickerOptions = {
         maxMode: 'month',
@@ -12,21 +16,28 @@ app.controller('ReservationController', function ReservationController($scope, $
         showWeeks: false,
         minDate: Date.now(),
     };
-    $scope.timePickerOptions = {
-    };
-    $scope.deleteReservation = function(reservationId) {
-        $http.post('/api/reservations/delete', { id: reservationId }).then(function() {
-            load();
-        })
-        .catch(function(err) {
-            $scope.error = 'Something went wrong.'
-        });
-    }
+
+    $scope.$watchGroup(['reservation.date', 'reservation.barber'], function(val) {
+        if(val[0] && val[1]) {
+            getTimes(val[0], val[1])
+        }
+    })
+
+    var timeStops = getTimeStops("10:00", "19:45");
+
+
 
    $scope.saveReservation = function() {
         $scope.error = null;
 
-       if (!$scope.reservation.name || !$scope.reservation.time) {
+        $scope.reservation.time = moment($scope.reservation.date);
+        $scope.reservation.time.set({
+            h: $scope.reservation.hour.split(':')[0],
+            m: $scope.reservation.hour.split(':')[1]
+        })
+
+
+       if (!$scope.reservation.hour) {
            $scope.error = "Įveskite vardą, bei pasirinkite laiką, bei kirpėją!";
            return;
        }
@@ -35,10 +46,56 @@ app.controller('ReservationController', function ReservationController($scope, $
         .then(function() {
             $scope.reservationSuccess = true;
         }).catch(function(err) {
-            $scope.error = 'Something went wrong.'
+            $scope.error = 'Something went wrong. Reservation exists.'
         });
-   } 
+   }
 
+   function getTimes(date, barber) {
+     $http
+       .post("/api/reservations/times", { date: date, barber: barber })
+       .then(function(times) {
+         $scope.times = times.data.map(function(time) {
+           return moment(time.time).format("HH:mm");
+         });
+
+         $scope.allTimes = timeStops.map(function(time) {
+           return {
+             time: time,
+             available: !$scope.times.includes(time)
+           };
+         });
+       });
+   }
+
+   function load() {
+
+    $http.get('/api/barbers').then(function(barbers) {
+        $scope.barbers = barbers.data;
+    }).catch(function(err) {
+        console.log(err);
+     })
+    }
+
+     load();
+
+     function getTimeStops(start, end) {
+       var startTime = moment(start, "HH:mm");
+       var endTime = moment(end, "HH:mm");
+
+       if (endTime.isBefore(startTime)) {
+         endTime.add(1, "day");
+       }
+
+       var timeStops = [];
+
+       while (startTime <= endTime) {
+         timeStops.push(new moment(startTime).format("HH:mm"));
+
+         startTime.add(15, "minutes");
+       }
+
+       return timeStops;
+     }
 });
 
 app.controller("BarberController", function BarberController($scope, $http, $q) {
@@ -53,6 +110,14 @@ app.controller("BarberController", function BarberController($scope, $http, $q) 
             load();
         })
         .catch(function(err) {
+            $scope.error = 'Something went wrong.'
+        });
+    }
+
+    $scope.deleteReservation = function(reservationId) {
+        $http.post('/api/reservations/delete', { id: reservationId}).then(function() {
+            load();
+        }).catch(function(err) {
             $scope.error = 'Something went wrong.'
         });
     }
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,19 +1,54 @@
 const router = require('express').Router();
 const wrap = require('express-async-wrap');
+const moment = require('moment');
 
 const Barber = require('../../schemas/Barber');
 const Reservation = require('../../schemas/Reservation');
 
-router.get('/reservations', wrap(async (req, res, next) => res.json(await Reservation.find())));
+router.get('/reservations', wrap(async (req, res, next) => {
+	const reservations = await Reservation.find().populate({
+		path: 'barber',
+		model: Barber
+	})
+	res.json(reservations)
+}));
 
 router.post('/reservations', wrap(async (req, res, next) => {
 	const reservation = req.body;
 
+	const exists = await Reservation.findOne({name: req.body.name, time: { $gte: Date.now() }});
+
+	if(exists) {
+		return res.sendStatus(403);
+	}
+
 	await Reservation.create(reservation);
-
-
 	res.sendStatus(200);
 }));
+
+router.post('/reservations/times', wrap(async (req, res, next) => {
+	const date = req.body.date;
+
+	const start = moment(date).startOf('day').format();
+	const end = moment(date).endOf('day').format();
+
+	const allReservations = await Reservation.find({
+		$and: [
+			{
+				time: { $gte: start }
+			},
+			{
+				time: { $lte: end }
+			},
+			{
+				barber: req.body.barber
+			}
+		]
+	}).lean();
+
+	res.json(allReservations);
+}));
+
 router.post("/reservations/delete", wrap(async (req, res, next) => {
 	await Reservation.findByIdAndDelete(req.body.id);
 	res.sendStatus(200);
